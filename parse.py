7b202034a81cf719b89608bacc62556aa76cc07c
Kehan Chen <71822884+KehanChen317@users.noreply.github.com>
2024-05-11 19:22:49 +0800

    Update parse.py

--- parse.py
+++ parse.py
@@ -3,16 +3,16 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("--gpu", type=str, help="input GPU id", default="0")
-    parser.add_argument("-t", "--target_platform", type=str, help="target_platform", default="BX_Bo")
-    parser.add_argument("-s", "--source_platform", type=str, help="source_platform", default="DB_Bo")
+    parser.add_argument("-s", "--source_platform", type=str, help="source_platform", default="DB")
+    parser.add_argument("-t", "--target_platform", type=str, help="target_platform", default="ML")
     parser.add_argument('--bpr_batch_size', type=int, help="bpr_batch_size", default=1024)
     parser.add_argument('-dim', '--latent_dim_rec', type=int, help="dim num", default=64)
     parser.add_argument('--lightGCN_n_layers', type=int, help="lightGCN_n_layers", default=3)
     parser.add_argument('--lr', type=float, help="the learning rate", default=0.005)
-    parser.add_argument('-rl','--reg_loss_weight', type=float, help="the weight of reg_loss", default=1e-3)
-    parser.add_argument('-pl','--prototype_loss_weight', type=float, help="the weight of prototype_loss", default=1e-1)
+    parser.add_argument('-rl','--reg_loss_weight', type=float, help="the weight of reg_loss", default=5e-4)
+    parser.add_argument('-pl','--prototype_loss_weight', type=float, help="the weight of prototype_loss", default=1e-2)
     parser.add_argument('--seed', type=int, help="seed", default=6298)
-    parser.add_argument('--epochs', type=int, help="the max epoch num", default=1000)
+    parser.add_argument('--epochs', type=int, help="the max epoch number", default=1000)
     parser.add_argument('--topks', type=list, help="topks", default=[10])
     parser.add_argument('--multicore', type=int, default=0, help='whether we use multiprocessing or not in test')
     parser.add_argument('--pretrained_model', type=int, help="whether to use pretrained_model", default=0)
@@ -22,4 +22,3 @@ def parse_args():
     parser.add_argument('-pw','--prototype_weight', type=float, help="user codebook weight", default=0.1)
     args = parser.parse_args()
     return args
-
